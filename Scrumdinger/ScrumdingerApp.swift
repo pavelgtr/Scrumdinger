b390e74e6290e19bcbd77cfef9be4948b0f39f1f
Pavel Rivera <pavelgtr@gmail.com>
2024-06-25 09:47:29 -0600

    first commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Pavel Rivera on 6/24/24.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
